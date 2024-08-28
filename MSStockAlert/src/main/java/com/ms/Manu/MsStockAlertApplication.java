937121084518f7dcef36b22f602df49cf799bd65
ManaiahJ <manaiah.j19988@gmail.com>
2024-08-28 21:29:42 +0530

    - Initial Commits as first time creating the project

---
+++ MSStockAlert/src/main/java/com/ms/Manu/MsStockAlertApplication.java
@@ -0,0 +1,13 @@
+package com.ms.Manu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStockAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStockAlertApplication.class, args);
+	}
+
+}
